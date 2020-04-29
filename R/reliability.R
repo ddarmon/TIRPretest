b50b169bc0b1cf2a19f65b322dd6db200f0e9ea7
David Darmon <gendarmon@gmail.com>
2020-04-29 15:40:13 -0400

    Add @export.

--- R/reliability.R
+++ R/reliability.R
@@ -1,45 +1,46 @@
+#' @export
 compute.reliability.measures <- function(dat, level = 0.95, B = 500){
-  
+
   # Reformat data for use with `psych`'s ICC() function,
   # which expects one row per-subject, and one column
   # per-measurement
-  
+
   X <- matrix(dat$measure, nrow = length(unique(dat$id)))
 
   ICC.out <- psych::ICC(X)
 
   icc.11 <- as.numeric(c(ICC.out$results[1, 2], ICC.out$results[1, 7:8]))
   icc.21 <- as.numeric(c(ICC.out$results[2, 2], ICC.out$results[2, 7:8]))
-  
+
   # Fit the ICC(1, 1) model which includes a single random effect
   # per-subject.
-  
+
   mod.icc11 <- lmer(measure ~ 1 + (1 | id), data = dat)
-  
+
   # Extract confidence intervals for \(\sigma_{\epsilon}\) from
   # the mixed effects model using either the profile likelihood
   # or bootstrapping.
 
   confint.icc11.prof <- confint(mod.icc11, level = level, method = 'profile', quiet = TRUE)
   confint.icc11.boot <- confint(mod.icc11, level = level, method = 'boot', nsim = B, quiet = TRUE)
-  
+
   # Compute the confidence intervals for SEM and MDD from the
   # confidence interval for \(\sigma_{\epsilon}\).
-  
+
   ci.icc11.sem.prof <- sqrt(2)*confint.icc11.prof[2, ]
   ci.icc11.mdd.prof <- qnorm(0.975)*sqrt(2)*confint.icc11.prof[2, ]
 
   ci.icc11.sem.boot <- sqrt(2)*confint.icc11.boot[2, ]
   ci.icc11.mdd.boot <- qnorm(0.975)*sqrt(2)*confint.icc11.boot[2, ]
-  
+
   # Compute the point estimates for SEM and MDD from the point
   # estimate for \(\sigma_{\epsilon}\).
-  
+
   sig.icc11 <- sigma(mod.icc11)
 
   icc11.sem <- sqrt(2)*sig.icc11
   icc11.mdd <- icc11.sem*qnorm(0.975)
-  
+
   # Store all of these results in a matrix.
 
   results <- matrix(NA, nrow = 5, ncol = 3)
@@ -62,7 +63,7 @@ compute.reliability.measures <- function(dat, level = 0.95, B = 500){
   rownames(results) <- c('ICC(1, 1)', 'SEM (Prof.)', 'SEM (Boot)', 'MDD (Prof.)', 'MDD (Boot)')
 
   ret <- list(icc11 = results)
-  
+
   # Function for use in bootstrapping from ICC(2, 1) model.
   mySumm <- function(.) {
     s <- sigma(.)
@@ -70,16 +71,16 @@ compute.reliability.measures <- function(dat, level = 0.95, B = 500){
 
     c(beta =getME(., "beta"), sigma = s, sig0 = sd.components, diff.var = 2*(s^2 + sd.components[2]))
   }
-  
+
   # Fit the ICC(2, 1) model which includes a random effect per-subject
   # and a random effect per-visit.
-  
+
   mod.icc21 <- lmer(measure ~ 1 + (1 | id) + (1 | visit), data = dat)
-  
+
   # Get point estimate for the SEM from mod.icc21.
-  
+
   params <- mySumm(mod.icc21)
-  
+
   # Bootstrap confidence intervals for the SEM.
 
   boot.out <-  bootMer(mod.icc21, mySumm, nsim = B)#, .progress = 'txt')
@@ -92,9 +93,9 @@ compute.reliability.measures <- function(dat, level = 0.95, B = 500){
 
   icc21.sem <- sqrt(params[5])
   icc21.mdd <- qnorm(0.975)*icc21.sem
-  
+
   # Store the results for the ICC(2, 1) model in a matrix.
-  
+
   results <- matrix(nrow = 3, ncol = 3)
 
   results[1, ] <- icc.21
